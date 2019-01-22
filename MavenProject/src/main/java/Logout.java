17b1fdd86fb8cc340a46c618436048fdf2bb2000
hp <hp@hp-PC>
2019-01-22 19:15:41 +0530

    first commit

---
+++ MavenProject/src/main/java/Logout.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Logout 
+{
+	WebDriver driver;
+	By logout=By.linkText("Log Out");
+	public Logout(WebDriver driver)
+	{
+		super();
+		this.driver = driver;
+	}
+	
+	public void clickOnLogOut() 
+	{
+		driver.findElement(logout).click();
+	}
+}
